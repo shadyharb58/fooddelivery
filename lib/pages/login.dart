72cda517b0ab0b6fab26378a7307ced15a5299f7
waelabohamza <waeleagle1243@gmail.com>
2020-10-29 17:24:00 +0300

    refresh

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -62,8 +62,7 @@ class _LoginState extends State<Login> {
     else {}
   }
 
-  Pattern pattern =
-      r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+   
 
   // Start Form Controller
 
